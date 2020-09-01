46ce7224684f9f52d02bb76c206849bb5ac1bcc3
Colin Osborn <colin.osborn@gmail.com>
2020-09-01 09:33:37 -0600

    search box built out and broken out to reusable component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list-component'
+import { SearchBox } from './components/search-box/search-box-component.'
+
 import './App.css';
 
-//Component also gives us set state
-//What set state does is, lets us modify the state object
 
 class App extends Component {
   constructor() {
     super(); 
       this.state = {
-        monsters: []        
+        monsters: [],
+        searchField:  ""        
       };
   }
 
@@ -20,9 +21,16 @@ class App extends Component {
   }
 
   render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter( monster => 
+      monster.name.toLowerCase().includes(searchField.toLowerCase()))
       return (
         <div className="App">
-          <CardList monsters={this.state.monsters}></CardList>
+            <SearchBox
+              placeholder="search monsters"
+              handleChange={ e => this.setState({ searchField: e.target.value })}>
+            </SearchBox>
+          <CardList monsters={ filteredMonsters }></CardList>
         </div>
       );
   }
