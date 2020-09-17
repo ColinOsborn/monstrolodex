c0247f4e9b0f98475178bbb08af32159be8428c3
Colin Osborn <colin.osborn@gmail.com>
2020-09-17 12:18:29 -0600

    last funcationality additions + styling

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { CardList } from './components/card-list/card-list-component'
-import { SearchBox } from './components/search-box/search-box-component.'
+import { SearchBox } from './components/search-box/search-box-component'
 
 import './App.css';
 
@@ -12,9 +12,16 @@ class App extends Component {
         monsters: [],
         searchField:  ""        
       };
+
+      this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(e) {
+    this.setState({ searchField: e.target.value })
   }
 
   componentWillMount() {
+    // lifecycle method
     fetch('https://jsonplaceholder.typicode.com/users')
     .then(response => response.json())
     .then(users => this.setState({ monsters: users }))
@@ -26,9 +33,10 @@ class App extends Component {
       monster.name.toLowerCase().includes(searchField.toLowerCase()))
       return (
         <div className="App">
+            <h1>Monsters Rolodex</h1>
             <SearchBox
               placeholder="search monsters"
-              handleChange={ e => this.setState({ searchField: e.target.value })}>
+              handleChange={ this.handleChange }>
             </SearchBox>
           <CardList monsters={ filteredMonsters }></CardList>
         </div>
